a586d2edc93cd0478b30245c0b78d492bcc8c85f
Anastasia1111 <25631532+Anastasia1111@users.noreply.github.com>
2018-12-30 17:46:00 +0700

    abstract classes2

---
+++ abstract/src/main/java/main/MUL.java
@@ -0,0 +1,13 @@
+package main;
+
+import java.util.*;
+import java.io.*;
+
+public class MUL extends Abstract {
+  public MUL(String str) {
+    super(str);
+  }
+  public int operation(int a, int b) {
+    return a * b;
+  }
+}
